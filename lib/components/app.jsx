492c117d3d374d23bd93289c533f5472258c1d73
Kuba Orlik <groovy354@gmail.com>
2015-10-16 14:18:12 +0200

    commit for realz

---
+++ lib/components/app.jsx
@@ -0,0 +1,9 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var MainContainer = require("./main-container.jsx");
+
+ReactDOM.render(
+	<MainContainer/>,
+	document.getElementById('main-container')
+);
